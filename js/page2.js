0d9e6cadc581033b0631504655cb1057655911d3
RyeSakurano <45529581+RyeSakurano@users.noreply.github.com>
2021-01-23 17:34:29 +0800

    Add files via upload

--- js/page2.js
+++ js/page2.js
@@ -1,5 +1,6 @@
 ﻿/* Javascript for page2 */
 let page2_tweet_data;
+let page2_wordlist;
 let page2_svg;
 let _width = $(window).width();
 let _height = $(window).height();
@@ -23,6 +24,14 @@ let timetext_red = [[new Date(2020, 1, 20), 'Call For Papers', 0],
                     [new Date(2020, 11, 6), 'Conference', 0]];
 let text_gray, text_red;
 let page2_y_attr;
+let text_x_fraction = (page2_width - page2_padding.left - page2_padding.right) / 4;
+let text_x_boundary = [page2_padding.left, page2_padding.left + text_x_fraction, page2_padding.left + 2 * text_x_fraction, page2_padding.left + 3 * text_x_fraction, page2_width - page2_padding.right];
+let text_y_boundary = [page2_padding.top + 0.3 * page2_height, page2_height - page2_padding.bottom];
+let current_x = {};
+let current_y = {};
+let wordsize = [20, 16, 14, 12, 12];
+let text_dev_x = [0, -50, +50, -25, +25];
+let text_dev_y = [0, +50, -50, -40, +40];
 
 let page2 = {
     //Your Content Here
@@ -32,13 +41,20 @@ let page2 = {
         objs.options[0].selected = true;
         var objs = document.getElementById("select_page2_timeline");
         objs.options[0].selected = true;
+        var objs = document.getElementById("select_page2_wordcloud");
+        objs.options[0].selected = true;
         d3.tsv('../data/tweet_data_20.csv').then(function (DATA_T) {
             page2_tweet_data = DATA_T;
+            d3.json('../data/wordlist.json').then(function (DATA_W) {
+                page2_wordlist = DATA_W;
+                //console.log(page2_wordlist);
+                page2.set_ui()
+                page2_svg = d3.select('#page2_container')
+                    .select('svg');
+                page2.draw('retweet-cnt');
+            })
             //console.log(page2_tweet_data);
-            page2.set_ui()
-            page2_svg = d3.select('#page2_container')
-                .select('svg');
-            page2.draw('retweet-cnt');
+            
         })
     },
 
@@ -57,6 +73,7 @@ let page2 = {
         xGroup.call(axis_x, xz);
         page2_svg.selectAll('circle')
             .attr('cx', (d, i) => {
+                current_x[d['tweet-id']] = xz(parseDate(d['time']));
                 return xz(parseDate(d['time']));
             })
             .on('mouseover', (e, d) => {
@@ -124,6 +141,77 @@ let page2 = {
                 else
                     return 'hidden';
             });
+
+        page2_svg.selectAll('.wordtext').remove();
+
+        var worddict = [[{}, {}, {}, {}]];
+        for (var key in current_x) {
+            if (current_x[key] < text_x_boundary[1]) {
+                    for (var item in page2_wordlist[key]) {
+                        if (worddict[0][0].hasOwnProperty(page2_wordlist[key][item]))
+                            worddict[0][0][page2_wordlist[key][item]] += 1;
+                        else
+                            worddict[0][0][page2_wordlist[key][item]] = 1;
+                    }
+             
+            }
+            else if (current_x[key] < text_x_boundary[2]) {
+              
+                    for (var item in page2_wordlist[key]) {
+                        if (worddict[0][1].hasOwnProperty(page2_wordlist[key][item]))
+                            worddict[0][1][page2_wordlist[key][item]] += 1;
+                        else
+                            worddict[0][1][page2_wordlist[key][item]] = 1;
+                    }
+                
+            }
+            else if (current_x[key] < text_x_boundary[3]) {
+                
+                    for (var item in page2_wordlist[key]) {
+                        if (worddict[0][2].hasOwnProperty(page2_wordlist[key][item]))
+                            worddict[0][2][page2_wordlist[key][item]] += 1;
+                        else
+                            worddict[0][2][page2_wordlist[key][item]] = 1;
+                    }
+                
+            }
+            else {
+                
+                    for (var item in page2_wordlist[key]) {
+                        if (worddict[0][3].hasOwnProperty(page2_wordlist[key][item]))
+                            worddict[0][3][page2_wordlist[key][item]] += 1;
+                        else
+                            worddict[0][3][page2_wordlist[key][item]] = 1;
+                    }
+                
+            }
+        }
+        g_text = page2_svg.append('g').attr('class', 'wordtext')
+            .attr('visibility', () => {
+                let idx = $("#select_page2_wordcloud option:selected").val();
+                if (idx == 0)
+                    return 'visible';
+                else
+                    return 'hidden';
+            });
+        for (var i = 0; i < 1; i++)
+            for (var j = 0; j < 4; j++) {
+                var obj = worddict[i][j];
+                var keys = Object.keys(obj);
+                var biggest = Math.max.apply(null, keys.map(function (x) { return obj[x] }));
+                if (biggest <= 3)
+                    continue;
+                console.log(biggest);
+                key = Object.keys(worddict[i][j]).sort(function (a, b) { worddict[i][j][b] - worddict[i][j][a] });
+                for (var k = 0; k < 5; k++) {
+                    g_text.append('text')
+                        .attr('x', (text_x_boundary[j] + text_x_boundary[j + 1]) / 2 + text_dev_x[k])
+                        .attr('y', (text_y_boundary[i] + text_y_boundary[i + 1]) / 2 + text_dev_y[k])
+                        .attr('font-size', wordsize[k])
+                        .text(key[k]);
+                }
+
+            }
     },
 
     draw(y_attr) {
@@ -187,10 +275,11 @@ let page2 = {
             .data(page2_tweet_data)
             .join('circle')
             .attr('cx', (d, i) => {
+                current_x[d['tweet-id']] = x(parseDate(d['time']));
                 return x(parseDate(d['time']));
             })
             .attr('cy', (d, i) => {
-                //console.log(parseFloat(d['emotion']));
+                current_y[d['tweet-id']] = y(parseFloat(d[y_attr]));
                 return y(parseInt(d[y_attr]));
             })
             .attr('r', 4)
@@ -218,7 +307,9 @@ let page2 = {
                 tooltip.select('.TweetAuthor-screenName')
                     .html('@' + d['user-screen-name']);
                 tooltip.select('.timeline-Tweet-text')
-                    .html(() => { console.log(d['text']); return d['text']; });
+                    .html(() => { //console.log(d['text']);
+                        return d['text'];
+                    });
                 tooltip.select('.timeline-Tweet-timestamp')
                     .html(function () {
                         return d3.timeFormat('%Y-%m-%d %H:%M:%S')(parseDate(d['time']))
@@ -255,7 +346,9 @@ let page2 = {
             .attr('class', 'timeline')
             .attr('y1', page2_padding.top - 30)
             .attr('y2', page2_height - page2_padding.bottom)
-            .attr('x1', (d, i) => { console.log(d); return x(d[0]); })
+            .attr('x1', (d, i) => { //console.log(d); 
+                return x(d[0]);
+            })
             .attr('x2', (d, i) => x(d[0]))
             .attr("stroke", "gray")
             .attr("stroke-width", 1.5)
@@ -275,7 +368,9 @@ let page2 = {
             .attr('class', 'timeline')
             .attr('y1', (d,i)=> page2_padding.top + d[1])
             .attr('y2', page2_height - page2_padding.bottom)
-            .attr('x1', (d, i) => { console.log(d); return x(d[0]); })
+            .attr('x1', (d, i) => { //console.log(d); 
+                return x(d[0]);
+            })
             .attr('x2', (d, i) => x(d[0]))
             .attr("stroke", "gray")
             .attr("stroke-width", 1.5)
@@ -294,7 +389,9 @@ let page2 = {
             .attr('class', 'timeline')
             .attr('y1', (d,i)=> page2_padding.top + d[1])
             .attr('y2', page2_height - page2_padding.bottom)
-            .attr('x1', (d, i) => { console.log(d); return x(d[0]); })
+            .attr('x1', (d, i) => { //console.log(d); 
+                return x(d[0]);
+            })
             .attr('x2', (d, i) => x(d[0]))
             .attr("stroke", "red")
             .attr("stroke-width", 1.5)
@@ -359,6 +456,75 @@ let page2 = {
             .attr('x', page2_padding.left * 0.5)
             .attr('y', d => (page2_padding.top * (0.3 + d[2])))
             .attr('font-size', 14);
+
+        var worddict = [[{}, {}, {}, {}]];
+        for (var key in current_x) {
+            if (current_x[key] < text_x_boundary[1]) {
+                for (var item in page2_wordlist[key]) {
+                    if (worddict[0][0].hasOwnProperty(page2_wordlist[key][item]))
+                        worddict[0][0][page2_wordlist[key][item]] += 1;
+                    else
+                        worddict[0][0][page2_wordlist[key][item]] = 1;
+                }
+
+            }
+            else if (current_x[key] < text_x_boundary[2]) {
+
+                for (var item in page2_wordlist[key]) {
+                    if (worddict[0][1].hasOwnProperty(page2_wordlist[key][item]))
+                        worddict[0][1][page2_wordlist[key][item]] += 1;
+                    else
+                        worddict[0][1][page2_wordlist[key][item]] = 1;
+                }
+
+            }
+            else if (current_x[key] < text_x_boundary[3]) {
+
+                for (var item in page2_wordlist[key]) {
+                    if (worddict[0][2].hasOwnProperty(page2_wordlist[key][item]))
+                        worddict[0][2][page2_wordlist[key][item]] += 1;
+                    else
+                        worddict[0][2][page2_wordlist[key][item]] = 1;
+                }
+
+            }
+            else {
+
+                for (var item in page2_wordlist[key]) {
+                    if (worddict[0][3].hasOwnProperty(page2_wordlist[key][item]))
+                        worddict[0][3][page2_wordlist[key][item]] += 1;
+                    else
+                        worddict[0][3][page2_wordlist[key][item]] = 1;
+                }
+
+            }
+        }
+        g_text = page2_svg.append('g').attr('class', 'wordtext')
+            .attr('visibility', () => {
+                let idx = $("#select_page2_wordcloud option:selected").val();
+                if (idx == 0)
+                    return 'visible';
+                else
+                    return 'hidden';
+            });
+        for (var i = 0; i < 1; i++)
+            for (var j = 0; j < 4; j++) {
+                var obj = worddict[i][j];
+                var keys = Object.keys(obj);
+                var biggest = Math.max.apply(null, keys.map(function (x) { return obj[x] }));
+                if (biggest <= 3)
+                    continue;
+                console.log(biggest);
+                key = Object.keys(worddict[i][j]).sort(function (a, b) { worddict[i][j][b] - worddict[i][j][a] });
+                for (var k = 0; k < 5; k++) {
+                    g_text.append('text')
+                        .attr('x', (text_x_boundary[j] + text_x_boundary[j + 1]) / 2 + text_dev_x[k])
+                        .attr('y', (text_y_boundary[i] + text_y_boundary[i + 1]) / 2 + text_dev_y[k])
+                        .attr('font-size', wordsize[k])
+                        .text(key[k]);
+                }
+
+            }
     },
 
     change_y_axis() {
@@ -382,7 +548,15 @@ let page2 = {
             page2_svg.selectAll('.timeline').style('visibility', 'hidden');
         }
             
-    }
+    }, 
+
+    change_wordcloud() {
+        let idx = $("#select_page2_wordcloud option:selected").val();
+        if (idx == 0)
+            page2_svg.select('.wordtext').attr('visibility', 'visible');
+        else
+            page2_svg.select('.wordtext').attr('visibility', 'hidden');
+    },
     
 }
 
